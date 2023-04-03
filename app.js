ed901410c537e62ef3e64745731a27486013f2c2
nedaashkan <117584245+nedaashkan@users.noreply.github.com>
2023-04-03 19:43:36 +0430

    added javascript

--- app.js
+++ app.js
@@ -1,67 +1,114 @@
 let shopItemsData = [
   {
-    id: "Chicken Egg Salad",
+    id: 1,
     name: "Chicken Egg Salad",
     price: 10,
     img: "images/1.PNG",
   },
   {
-    id: "Hot Grid Chicken",
+    id: 2,
     name: "Hot Grid Chicken",
     price: 20,
     img: "images/2.PNG",
   },
   {
-    id: "Salmon Salad",
+    id: 3,
     name: "Salmon Salad",
     price: 35,
     img: "images/3.PNG",
   },
   {
-    id: "Soup",
+    id: 4,
     name: "Soup",
     price: 25,
     img: "images/4.PNG",
   },
   {
-    id: "Salad",
+    id: 5,
     name: "Salad",
     price: 15,
     img: "images/5.PNG",
   },
   {
-    id: "Pizza",
+    id: 6,
     name: "Pizza",
     price: 10,
     img: "images/6.PNG",
   },
 ];
+const shop = document.getElementById("shop");
+let shoppingCart = [];
 
-const shopEl = document.getElementById("shop");
-shopEl.in;
 let generateShop = () => {
-  let shop = "";
+  let shopEl = "";
   shopItemsData.map((x, y) => {
     const { id, name, price, img } = x;
-    return (shop = `
-          <div class="item">
-        <img src="${img}" />
-        <div class="details">
-          <div class="title">${name}</div>
-          <div class="price-quantity">
-            <div class="price">$10</div>
-            <div class="buttons">
-              <i class="bi bi-dash-lg"></i>
-              <div class="quantity">0</div>
-              <i class="bi bi-plus-lg"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    
-    
-    
-    `);
+    let search = shoppingCart.find((x) => x.id === id) || [];
+    return (shopEl += `
+       <div class="item" id=${y}>
+         <img src="${img}" />
+         <div class="details">
+           <div class="title">${name}</div>
+           <div class="price-quantity">
+             <div class="price">$${price}</div>
+             <div class="buttons">
+               <i class="bi bi-dash-lg" onclick="decrement(${id})"></i>
+               <div class="quantity" id=${id}> ${
+      search.item === undefined ? 0 : search.item
+    }</div>
+               <i class="bi bi-plus-lg"  onclick="increment(${id})"></i>
+             </div>
+           </div>
+         </div>
+       </div>
+`);
   });
+
+  return (shop.innerHTML = shopEl);
 };
+
+generateShop();
+
+let increment = (id) => {
+  let search = shoppingCart.find((x) => x.id === id);
+  if (search === undefined) {
+    shoppingCart.push({
+      id: id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  localStorage.setItem("data", JSON.stringify(shoppingCart));
+  update(id);
+};
+let decrement = (id) => {
+  let search = shoppingCart.find((x) => x.id === id);
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  localStorage.setItem("data", JSON.stringify(shoppingCart));
+  update(id);
+};
+
+let update = (id) => {
+  let search = shoppingCart.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = shoppingCart
+    .map((x) => x.item)
+    .reduce((x, y) => x + y, 0);
+};
+
+(() => {
+  shoppingCart = JSON.parse(localStorage.getItem("data")) || [];
+  generateShop();
+})();
+
+calculation();
